1884ec5ba59c960514f8d1a209dd76b91ce8ebfd
Unknown <andrsolo21@gmail.com>
2018-12-13 22:40:15 +0300

    some changes

---
+++ M2/lab_1/inf/прогр.py
@@ -0,0 +1,14 @@
+import re
+def preobr(name):
+    f = open(name)
+    s = f.read()
+    f.close()    
+    s = re.sub('\n', ' ',s)
+    s = re.sub('\t', ' ',s)
+    sb = s.split()
+    s = re.sub(',', '.',s)
+    f = open("new_" + name, 'w')
+    f.write(s)
+    f.close()
+    
+    
